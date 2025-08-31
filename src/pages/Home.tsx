e7488bbf7be59b7b1c761e393425beeb01c86cc5
Artem <artiebears@mail.ru>
2025-08-31 15:36:37 +0300

    deploy

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,3 @@
 export function Home() {
-    return <h1 style={{marginTop: '50px', marginLeft: '100px'}}>Главная</h1>;
+    return <h1 style={{marginTop: '30px', marginLeft: '80px'}}>Главная</h1>;
 }
